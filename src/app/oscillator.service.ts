5635ba8aea991a321f866bd3b4907b03871b39f0
Christoph Franke <krito@krito.de>
2019-05-10 17:06:43 +0200

    refactored oscillator to follow a more concise concept

--- src/app/oscillator.service.ts
+++ src/app/oscillator.service.ts
@@ -26,6 +26,10 @@ export class OscillatorService {
 	}
 
 	set mode(value) {
+		if (value === this._mode) {
+			return
+		}
+
 		if (this._mode === 'mono') {
 			this.downNotes.forEach(({ pitch }) => this.noteOff(pitch))
 		}
@@ -36,50 +40,76 @@ export class OscillatorService {
 		this._mode = value
 	}
 
+	note(pitch) {
+		const osc = this.audioService.context.createOscillator()
+		const output = this.output.note(osc)
+		const lfo = this.audioService.context.createOscillator()
+		lfo.frequency.value = this.detune.frequency
+		const lfoGain = this.audioService.context.createGain()
+		lfoGain.gain.value = this.detune.amount
+
+		const noteOn = () => {
+			osc.frequency.value = frequency(pitch)
+			osc.type = <OscillatorType>this.waveform
+			osc.start()
+
+			if (this.detune.amount > 0) {			
+				lfo.connect(lfoGain)
+				lfoGain.connect(osc.detune)
+				lfo.start()
+			}
+			output.noteOn()
+		}
+		const noteOff = () => {
+			output.noteOff(() => {
+				osc.stop()
+				lfo.stop()
+				lfo.disconnect(lfoGain)
+				lfoGain.disconnect(osc.detune)
+			})
+		}
+		const changeTo = (newPitch = pitch) => {
+			osc.frequency.value = frequency(newPitch)
+			return {
+				noteOn,
+				noteOff,
+				changeTo,
+				pitch: newPitch
+			}
+		}
+
+		return {
+			noteOn,
+			noteOff,
+			changeTo,
+			pitch
+		}
+	}
+
 	noteOn(pitch) {
 		if (!this.tones[pitch]) {
-			let osc, output
+			let output
 			if (this.mode === 'poly' || this.downNotes.length === 0) {			
-				osc = this.audioService.context.createOscillator()
-				output = this.output.note(osc)
-				osc.frequency.value = frequency(pitch)
-				osc.type = <OscillatorType>this.waveform
-				osc.start()
-
-				if (this.detune.amount > 0) {			
-					const lfo = this.audioService.context.createOscillator()
-					lfo.frequency.value = this.detune.frequency
-					const lfoGain = this.audioService.context.createGain()
-					lfoGain.gain.value = this.detune.amount
-					lfo.connect(lfoGain)
-					lfoGain.connect(osc.detune)
-					lfo.start()
-				}
+				output = this.note(pitch)
 				output.noteOn()
-
-				this.downNotes.push({ osc, output, pitch })
 			} else {
-				osc = this.downNotes[0].osc
-				output = this.downNotes[0].output
-				osc.frequency.value = frequency(pitch)
-				this.downNotes.push({ osc, output, pitch })
+				output = this.downNotes[this.downNotes.length - 1].changeTo(pitch)
 			}
-			this.tones[pitch] = { osc, output }
+			this.downNotes.push(output)
+			this.tones[pitch] = output
 		} 
 	}
 
 	noteOff(pitch) {
 		if (this.tones[pitch]) {
-			if (this.mode === 'poly' || this.downNotes.length === 1) {			
-				const { osc, output } = this.tones[pitch]
-				output.noteOff(() => {
-					osc.stop()
-				})
-				this.downNotes = []
+			if (this.mode === 'poly' || this.downNotes.length === 1) {
+				this.tones[pitch].noteOff()
 			} else {
-				this.downNotes = this.downNotes.filter(n => n.pitch !== pitch)
-				this.downNotes[0].osc.frequency.value = frequency(this.downNotes[this.downNotes.length - 1].pitch)
+				if (this.downNotes[this.downNotes.length - 1].pitch === pitch) {
+					this.downNotes[this.downNotes.length - 2].changeTo()
+				}
 			}
+			this.downNotes = this.downNotes.filter(note => note.pitch !== pitch)
 			delete this.tones[pitch]
 		}
 	}
